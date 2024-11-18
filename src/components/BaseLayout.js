f5ded1fc495e97e484cefd716ede40d4b698112e
Chetan Menaria <93630545+chetanmenaria00@users.noreply.github.com>
2024-11-18 12:23:18 +0530

    Links update

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -49,7 +49,7 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                   py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
-                  <p>Created with &hearts; by <a href={'https://paytonpierce.dev'}>Chetan Menaria</a></p>
+                  <p>Created with &hearts; by Chetan Menari</p>
                   <p>&copy; 2024</p>
                </Box>
             </Grid>
